ab586dfa7cffcc3443e89d38948f13356d89b771
Gopal Menon <gopal.mnn@gmail.com>
2016-01-25 22:49:55 -0700

    Fixed source to parse tweets

--- testJson.py
+++ testJson.py
@@ -1,5 +1,6 @@
 import json
 
+# Retrieve tweets from text file
 trump_tweets_json = []
 
 for tweet_line in open('clintonTweets2.txt'):
@@ -8,17 +9,41 @@ for tweet_line in open('clintonTweets2.txt'):
   except:
     pass
 
-#print json.dumps(trump_tweets_json[0]['statuses'][0], indent = 5)
-# This is the first tweet extracted
-print trump_tweets_json[0]['statuses'][0]['created_at']
-print trump_tweets_json[0]['statuses'][0]['id_str']
-print trump_tweets_json[0]['statuses'][0]['text']
-print trump_tweets_json[0]['statuses'][0]['user']['screen_name']
-print trump_tweets_json[0]['statuses'][0]['user']['id_str']
-print trump_tweets_json[0]['statuses'][0]['user']['location']
-print trump_tweets_json[0]['statuses'][0]['place']
-print trump_tweets_json[0]['statuses'][0]['user']['friends_count']
-print trump_tweets_json[0]['statuses'][0]['user']['followers_count']
-print trump_tweets_json[0]['statuses'][0]['user']['lang']
- 
+# List to hold parsed tweets
+all_tweets = []
+
+# Template for parsed tweet
+class tweet_details:
+	pass
+
+# Total number of tweets retrieved from text file
+number_of_tweets =  len(trump_tweets_json[0]['statuses'])
+
+# Fill list with parsed tweets
+for count in range(0, number_of_tweets):
+	current_tweet = tweet_details()
+	current_tweet.created_at = trump_tweets_json[0]['statuses'][count]['created_at']
+	current_tweet.id_str = trump_tweets_json[0]['statuses'][count]['id_str']
+	current_tweet.text = trump_tweets_json[0]['statuses'][count]['text']
+	current_tweet.user_screen_name = trump_tweets_json[0]['statuses'][count]['user']['screen_name']
+	current_tweet.user_id_str = trump_tweets_json[0]['statuses'][count]['user']['id_str']
+	current_tweet.user_location = trump_tweets_json[0]['statuses'][count]['user']['location']
+	current_tweet.place = trump_tweets_json[0]['statuses'][count]['place']
+	current_tweet.user_friends_count = trump_tweets_json[0]['statuses'][count]['user']['friends_count']
+	current_tweet.user_followers_count = trump_tweets_json[0]['statuses'][count]['user']['followers_count']
+	current_tweet.user_lang = trump_tweets_json[0]['statuses'][count]['user']['lang']
+	all_tweets.append(current_tweet)
+
+# Print parsed tweets
+for count in range(0, len(all_tweets)):
+	print all_tweets[count].created_at
+	print all_tweets[count].id_str
+	print all_tweets[count].text
+	print all_tweets[count].user_screen_name
+	print all_tweets[count].user_id_str
+	print all_tweets[count].user_location
+	print all_tweets[count].place
+	print all_tweets[count].user_friends_count
+	print all_tweets[count].user_followers_count
+	print all_tweets[count].user_lang
 
